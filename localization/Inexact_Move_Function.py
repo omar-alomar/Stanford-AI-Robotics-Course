e05b8cebf81f8349ffbefafee0c6c3774fea3305
Omar Alomar <oaloma1@students.towson.edu>
2023-08-31 18:29:45 -0400

    inexact move fn

--- localization/Inexact_Move_Function.py
+++ localization/Inexact_Move_Function.py
@@ -1,12 +1,15 @@
-#Program a function that returns a new distribution 
-#q, shifted to the right by U units. If U=0, q should 
-#be the same as p.
+#Modify the move function to accommodate the added 
+#probabilities of overshooting or undershooting 
+#the intended destination.
 
 p=[0, 1, 0, 0, 0]
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q=[]
@@ -19,12 +22,14 @@ def sense(p, Z):
     return q
 
 def move(p, U):
-    temp = 0
-    q = [0] * 5
-    q[0] = p[0]
-    for i in range(len(p)-1):
-        temp = p[i]
-        q[i+U] = temp
+    q = []
+    for i in range(len(p)):
+        q.append(
+            pExact * p[(i-U)%len(p)] +
+            pUndershoot * p[(i-U - 1)%len(p)] + 
+            pOvershoot * p[(i-U+1)%len(p)]
+            )
     return q
+    
 
 print(move(p, 1))
